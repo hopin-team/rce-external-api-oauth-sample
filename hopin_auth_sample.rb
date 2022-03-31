69d648c63b05a898f3131ecc3cf3df9185f69e0f
Daniel Cooper <daniel@14lines.com>
2022-03-31 12:29:50 +0100

    Update example to use RI set via the intention

--- hopin_auth_sample.rb
+++ hopin_auth_sample.rb
@@ -5,6 +5,8 @@ require 'erb'
 require 'json'
 require 'rest-client'
 require 'sinatra'
+require "sinatra/reloader"
+require "sinatra/cookies"
 
 include ERB::Util
 
@@ -14,9 +16,8 @@ APP_ID       = ENV['HOPIN_APP_ID']
 APP_SECRET   = ENV['HOPIN_APP_SECRET']
 CALLBACK     = url_encode(ENV['HOPIN_CALLBACK_URL']) # This must match exactly the callback url that hopin has on file for this app.
 PLATFORM_URI = ENV.fetch("HOPIN_PLATFORM_DOMAIN", "https://hopin.com/")
-API_URI      =  ENV.fetch("HOPIN_API_DOMAIN", "https://api.hopin.com/")
+API_URI      = ENV.fetch("HOPIN_API_DOMAIN", "https://api.hopin.com/")
 SCOPES       = 'api'
-RESOURCE     = url_encode(ENV['HOPIN_RESOURCE_URI'])
 
 get '/' do
   erb :index
@@ -24,7 +25,7 @@ end
 
 get '/auth' do
   state = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
-  redirect to("#{PLATFORM_URI}/oauth/authorize?&redirect_uri=#{CALLBACK}&client_id=#{APP_ID}&scope=#{SCOPES}&state=#{state}&response_type=code&resource=#{RESOURCE}")
+  redirect to("#{PLATFORM_URI}/oauth/authorize?&redirect_uri=#{CALLBACK}&client_id=#{APP_ID}&scope=#{SCOPES}&state=#{state}&response_type=code&resource=#{cookies[:resource]}")
 end
 
 get '/auth/hopin/callback' do
@@ -38,16 +39,23 @@ get '/auth/hopin/callback' do
   else
     # Exchange our new auth code for an access_token. You'll notice it has a "expires_at" attribute. When this time elapses
     # the access_token will no longer work. You should use the refresh token to get a new pair of refresh/access tokens in that case.
-    response = RestClient.post "#{PLATFORM_URI}oauth/token?grant_type=authorization_code&code=#{auth_code}&redirect_uri=#{CALLBACK}&client_id=#{APP_ID}&client_secret=#{APP_SECRET}&resource=#{RESOURCE}", {},
+    response = RestClient.post "#{PLATFORM_URI}oauth/token?grant_type=authorization_code&code=#{auth_code}&redirect_uri=#{CALLBACK}&client_id=#{APP_ID}&client_secret=#{APP_SECRET}&resource=#{cookies[:resource]}", {},
                                { Accept: 'application/json' }
     token_details = JSON.parse(response.body)
 
     # Use the access token check the healthcheck endpoint.
-    response = RestClient.get "#{API_URI}v1/health", { Authorization: bearer_token(token_details['access_token']) }
+    response = RestClient.get "#{API_URI}v1/organizations", { Authorization: bearer_token(token_details['access_token']) }
     api_response = JSON.parse(response.body)
 
     erb :authed, locals: { api_response: api_response, token_details: token_details }
   end
+end
+
+# Receive the resource from the install intention path. Here we're just storing it in the cookies but in your real
+# app you'll want to put it somewhere more robust.
+get '/install' do
+  cookies[:resource] = params['resource']
+  redirect to('/')
 end
 
 def bearer_token(token)
